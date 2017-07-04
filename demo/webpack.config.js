a767fe82c92d6fd7d7b34660a44803630c542173
linhao <linhao.4zhong@163.com>
2017-07-04 21:43:49 +0800

    更改开发方式

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,3 +1,11 @@
+/**
+ * 如何开发？
+ * 第一步，进入watch监听src变化 znpm run watch
+ * 第二部，开启webpack-dev-server服务 znpm run server
+ * 更改src的文件导致watch输出文件到dist
+ * dist文件改动会触发webpack-dev-server的刷新
+ * **/
+
 const path = require( 'path' );
 const ExtractTextPlugin = require( "extract-text-webpack-plugin" );
 const webpack = require( 'webpack' );
@@ -10,7 +18,10 @@ const extractSass = new ExtractTextPlugin( {
 module.exports = {
     devtool: 'source-map',
     entry: {
-        Button: path.resolve( __dirname, './Button/src/index.js' ),
+        Button: [
+            path.resolve( __dirname, './Button/src/index.js' ),
+            'webpack-dev-server/client?http://localhost:9000/',
+        ],
     },
 
     output: {
@@ -20,7 +31,6 @@ module.exports = {
 
     devServer: {
         contentBase: path.join( __dirname ),
-        publicPath: "/Button/",
         compress: true,
         port: 9000,
         inline:true
